d108566304f7bc5c9c50f23bce01f53e24f1d02b
KOLAGANI_ABISHEK_VTU27981 <vtu27981@veltech.edu.in>
2026-08-05 09:35:16 +0530

    Update task3.java
    Removed unnecessary whitespace and improved formatting.

--- task3.java
+++ task3.java
@@ -4,27 +4,21 @@ import java.util.Scanner;
 public class BinarySearch {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
-        
         System.out.print("Enter the number of elements: ");
         int n = sc.nextInt();
-
         int[] arr = new int[n];
         System.out.println("Enter the sorted array elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-
         System.out.print("Enter the element to search: ");
         int key = sc.nextInt();
-
         int low = 0;
         int high = n - 1;
         int found = -1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
-
             if (arr[mid] == key) {
                 found = mid;
                 break;
