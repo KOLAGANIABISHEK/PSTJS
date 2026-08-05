0892dc2c89cd7787074b51e09eccef758a1ff3aa
KOLAGANI_ABISHEK_VTU27981 <vtu27981@veltech.edu.in>
2026-08-05 09:38:55 +0530

    Update task9.java

---
+++ task9.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+@FunctionalInterface
+interface palindrome {
+    boolean check(int number);
+}
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt(); 
+        palindrome p = n -> {
+            int original = n;
+            int reverse = 0;
+            while(n > 0) {
+                int digit = n % 10;
+                reverse = reverse * 10 + digit;
+                n /= 10;
+            }
+            return original == reverse;
+
+        };
+        if (p.check(number)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a palindrome");
+        }
+        sc.close();
+    }
+}
