a6fce02cdebb78de12f652bc148d265f0e42d474
KOLAGANI_ABISHEK_VTU27981 <vtu27981@veltech.edu.in>
2026-08-05 09:34:09 +0530

    Clean up task2.java by removing blank lines
    Removed unnecessary blank lines and added a newline at the end of the file.

--- task2.java
+++ task2.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        
-
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the no of elements in the array: ");
         int  n = sc.nextInt();
@@ -15,13 +12,10 @@ public class Main {
         for(int i=0;i<n;i++){
             arr[i] = sc.nextInt();
         }
-
-
         System.out.print("Enter the index number: ");
         int index = sc.nextInt();
 
         System.out.println("The element is " + arr[index]);
-
         sc.close();
     }
 }
