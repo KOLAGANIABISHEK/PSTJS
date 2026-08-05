6426f2b0abff01cd1942cf5089f73a6248b4dd2d
KOLAGANI_ABISHEK_VTU27981 <vtu27981@veltech.edu.in>
2026-08-05 09:35:51 +0530

    Update Task4.java

--- Task4.java
+++ Task4.java
@@ -1,14 +1,10 @@
 //Task 4 : To Find the maximum element in an array ;
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int arr[] = {12, 20, 30, 40, 50};
         int max = arr[0];
-
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > max) {
                 max = arr[i];
@@ -16,7 +12,6 @@ public class Main {
         }
 
         System.out.println("Maximum element is: " + max);
-
         sc.close();
     }
 }
