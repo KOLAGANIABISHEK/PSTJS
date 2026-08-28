1a996da9bfd98e3170870bc26bd1fe002cd6ef81
KOLAGANI_ABISHEK_VTU27981 <vtu27981@veltech.edu.in>
2026-08-28 10:23:32 +0530

    Add files via upload

---
+++ W4_movezeros.java
@@ -0,0 +1,14 @@
+/* Move Zeroes https://leetcode.com/problems/move-zeroes/description/ */
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+        }
+    }
+}
