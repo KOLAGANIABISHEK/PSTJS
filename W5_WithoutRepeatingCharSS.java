214522208c281f3c4eb9142745e040a1f04666c6
KOLAGANI_ABISHEK_VTU27981 <vtu27981@veltech.edu.in>
2026-09-16 09:30:39 +0530

    Add files via upload

---
+++ W5_WithoutRepeatingCharSS.java
@@ -0,0 +1,18 @@
+/* Longest Substring Without Repeating Characters https://leetcode.com/problems/longest-substring-without-repeating-characters/description/*/ 
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int left = 0;
+        int maxLength = 0;
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
